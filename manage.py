43a66924a3d275d4270449aeb51b1daffd95ff4e
gukurhgodp31 <wushangza19931030@126.com>
2021-05-26 17:04:42 +0800

    正式开始

--- manage.py
+++ manage.py
@@ -1,10 +1,22 @@
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+class Config():
+
+    DEBUG = True
+
+
+    ALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-    return 'index'
+
+app.config.from_object(Config)
+
 
 
 if __name__ == '__main__':
